33bb5aa27fad9a4a849f3840a28c3336332259ab
michsgithub <michen914@yahoo.com>
2019-01-30 09:37:14 -0800

    deleting files

---
+++ precache-manifest.42b3b92af856b141ab3ee7aa149107fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f91792709a152b0ffe0a",
+    "url": "/static/css/main.1c0839b5.chunk.css"
+  },
+  {
+    "revision": "f91792709a152b0ffe0a",
+    "url": "/static/js/main.f9179270.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "abcd411a063a643335cfbd495e5af66a",
+    "url": "/index.html"
+  }
+];
